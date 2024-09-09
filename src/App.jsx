b1e2ca8d9c905b278722ddb42839ed8ce8009329
silpakammara99 <silpakammara9702@gmail.com>
2024-09-09 11:39:24 +0530

    adding data into cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import PresentCards from "./task1/ppt-task"
-import { FormData } from "./Task2/formData"
+import { FormData } from "./Task-3/formData"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app=()=>{
